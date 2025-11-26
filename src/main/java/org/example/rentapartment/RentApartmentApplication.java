20711dc4c597e6d9f442e73c977274bc033760cf
Samoilenko Mariia <mariia.samoilenko.06@gmail.com>
2025-11-26 22:27:19 +0200

    add project structure

---
+++ src/main/java/org/example/rentapartment/RentApartmentApplication.java
@@ -0,0 +1,13 @@
+package org.example.rentapartment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RentApartmentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RentApartmentApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/rentapartment/RentApartmentApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.rentapartment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RentApartmentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
